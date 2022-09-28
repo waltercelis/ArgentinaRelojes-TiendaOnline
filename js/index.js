d1ba291b4487efd48cbb6388651fac36987221c7
Walter Celis <waltescelir22@gmail.com>
2022-09-28 20:16:47 -0300

    haciendo cambios en el js, localstorage y agregando barra de busqueda

--- js/index.js
+++ js/index.js
@@ -2,25 +2,25 @@ const productos = [
   {
     id: 1,
     title: "Reloj Verde",
-    price: 60000,
+    precio: 69000,
     img: "./img/relojHombreVerde.webp",
   },
   {
     id: 2,
     title: "Reloj Dorado",
-    price: 79090,
+    precio: 179090,
     img: "./img/relojHombreDorado.webp",
   },
   {
     id: 3,
     title: "Reloj Negro",
-    price: 58000,
+    precio: 110000,
     img: "./img/relojHombreNegro.webp",
   },
   {
     id: 4,
     title: "Reloj Azul",
-    price: 55000,
+    precio: 145999,
     img: "./img/relojHombreAzul.webp",
   },
 ];
@@ -37,7 +37,7 @@ function listarProductos() {
                 <div class="separador">
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum voluptates eius perspiciatis laudantium molestiae error animi, dolorem veniam porro totam modi fugit est qui maiores enim. Recusandae optio fugiat inventore?</p>
                     <div class="producto__info">
-                        <h3>$${producto.price}</h3>
+                        <h3>$${producto.precio}</h3>
                         <h3>${producto.title}</h3>
                         <h4>Hombre</h4>
                         <button class="btn" onclick="agregarProductosCarrito(${producto.id})">Agregar al carrito</button>
@@ -54,7 +54,6 @@ listarProductos();
 function agregarProductosCarrito(id) {
   let producto = productos.find((producto) => producto.id === id);
   console.log(producto.id);
-
   let productoCarrito = carrito.find((producto) => producto.id === id);
 
   if (productoCarrito) {
@@ -63,15 +62,9 @@ function agregarProductosCarrito(id) {
     producto.cantidad = 1;
     carrito.push(producto);
   }
-
-  /* let can = localStorage.cantidad; */
-
-  /* console.log("can", can); */
-  let nuevoProducto = localStorage.setItem("nuevoProducto", JSON.stringify(carrito));
-  /* let can = "nuevoProducto", carrito.cantidad;
-  console.log("can", can); */
   
   console.log(carrito);
+  
   listarCarrito();
   totalCarrito();
 }
@@ -79,11 +72,7 @@ function agregarProductosCarrito(id) {
 function listarCarrito() {
   console.log(carritoHTML);
   let htmlCarrito = "";
-  let carritoStorage = JSON.parse(localStorage.getItem("nuevoProducto"));
-   
-  if(carritoStorage){
-    carrito = carritoStorage;
-  }
+  ticket.innerHTML = "";
 
   carrito.forEach((prod, id) => {
     htmlCarrito += `
@@ -91,7 +80,7 @@ function listarCarrito() {
         <li>
             <a class="a-producto"><img src="${prod.img}" alt="Reloj"></a>
             <div class="producto__info">
-                <h3>$${prod.price}</h3>
+                <h3>$${prod.precio}</h3>
                 <h3>${prod.title}</h3>
                 <h4>Hombre</h4>
                 <p>Cantidad: ${prod.cantidad}</p>
@@ -102,13 +91,17 @@ function listarCarrito() {
     `;
   });
   carritoHTML.innerHTML = htmlCarrito;
+  localStorage.setItem("nuevoProducto", JSON.stringify(carrito));
+  if(localStorage.nuevoProducto === "[]") {
+    localStorage.clear();
+  }
 }
 
 function totalCarrito() {
   let total = 0;
 
   carrito.forEach((prod) => {
-    total += prod.price * prod.cantidad;
+    total += prod.precio * prod.cantidad;
   });
 
   console.log(total);
@@ -136,11 +129,9 @@ function eliminarProductosCarrito(id) {
   let productoStorage = cantt;
   console.log(productoStorage); */
   /* localStorage.removeItem("nuevoProducto.cantidad"); */
-  localStorage.removeItem("nuevoProducto");
 
   if (carrito[id].cantidad === 0) {
     carrito.splice(id, 1);
-    localStorage.removeItem("nuevoProducto");
   }
   
   listarCarrito();
@@ -150,7 +141,6 @@ function eliminarProductosCarrito(id) {
 function vaciarCarrito() {
   const ticket = document.querySelector("#ticket");
   ticket.innerHTML = "";
-  localStorage.clear();
   carrito = [];
   listarCarrito();
   totalCarrito();
@@ -161,17 +151,21 @@ function comprarCarrito() {
   let title = "";
   let cantidad = 0;
   let ticketforeach = "";
+  let ticketforeach2 = "";
   carrito.forEach((prod) => {
     ticketforeach = `
     <h4>Ticket</h4>
-    <h5>$${tot += prod.price * prod.cantidad}</h5>
+    <h5>total: $${tot += prod.precio * prod.cantidad}</h5>
     `
-    {/* <h6>${title += prod.title}</h6>
-    <h7>${cantidad += prod.cantidad}</h7> */}
+    ticketforeach2 += `
+    <h6>${title = prod.title}</h6>
+    <h7>$${tot = prod.precio * prod.cantidad}</h7>
+    <h8>${cantidad = prod.cantidad}</h8>
+    `
   });
   if(tot != 0) {
     const ticket = document.querySelector("#ticket");
-    ticket.innerHTML = ticketforeach;
+    ticket.innerHTML = ticketforeach + ticketforeach2;
   }
 }
 
@@ -186,3 +180,86 @@ vaciar.addEventListener("click", vaciarCarrito);
 
 const comprar = document.querySelector("#comprar");
 comprar.addEventListener("click", comprarCarrito);
+
+const formBuscar = document.querySelector("#form-buscar");
+formBuscar.addEventListener('submit', (e) => {
+  e.preventDefault()
+})
+
+const buscar = document.querySelector("#buscar");
+buscar.addEventListener('keyup', e => {
+  let msg = "";
+  let cont = 0;
+
+  for (let i = 0; i < productos.length; i++) {
+    const element = productos[i];
+    let en = element.title.toString().toLowerCase();
+    let sp = buscar.value.toString().toLowerCase();
+    if(en.includes(sp)) {
+      msg += show(element, i);
+      cont++;
+    } 
+  }
+  items.innerHTML = msg;
+});
+
+function show(product) {
+  let msg = `
+  <ul class="section__img">
+      <li>
+          <a class="a-producto"><img src="${product.img}" alt="Reloj"></a>
+          <div class="separador">
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum voluptates eius perspiciatis laudantium molestiae error animi, dolorem veniam porro totam modi fugit est qui maiores enim. Recusandae optio fugiat inventore?</p>
+              <div class="producto__info">
+                  <h3>$${product.price}</h3>
+                  <h3>${product.title}</h3>
+                  <h4>Hombre</h4>
+                  <button class="btn" onclick="agregarProductosCarrito(${product.id})">Agregar al carrito</button>
+              </div>
+          </div>
+      </li>
+  </ul>
+  `;
+  return msg;
+};
+
+/* let precioRango = `
+  <h4>Rango de precio</h4>
+  <form  class="precioRange">
+      <label for="rangeSlider_inversed">Mínimo:</label>
+      <input id="rangeSlider_inversed" type="range" min="50000" max="200000" value="50000"></input>
+      <output>50000</output>
+      <label for="rangeSlider">Máximo:</label>
+      <input id="rangeSlider" type="range" min="50000" max="200000" value="200000"></input>
+      <output>200000</output>
+      <input type="submit" value="Aplicar">
+  </div>
+  `;
+  
+function filterRange(arr, a, b) {
+  return arr.filter(item => (a <= item && item <= b));
+}
+
+const filtroPrecio = document.querySelector("#filtroPrecio");
+filtroPrecio.innerHTML = precioRango;
+const filtrarPrecio = filtroPrecio.on('submit', 'form.precioRange', (e) => {
+  let precioEnValueMayor = e.target[2].value;
+  console.log(precioEnValueMayor)
+  let precioEnValueMenor = e.target[0].value;
+
+  for (const producto of productos) {
+    let precioEnProducto = $(producto).children('ul')[0].lastElementChild.innerHTML;
+    if (parseInt(precioEnProducto) > parseInt(precioEnValueMenor) && parseInt(precioEnProducto) < parseInt(precioEnValueMayor)) { 
+    } else {
+      producto.remove();
+    }
+  }
+});
+//CAMBIAMOS EL VALOR DE OUTPUT PARA QUE CAMBIE CON EL CALUE 
+//DEL INPUT
+filtroPrecio.on('input', '#rangeSlider', (e) => {
+  e.target.nextElementSibling.value = e.target.value
+}); 
+filtroPrecio.on('input', '#rangeSlider_inversed', (e) => {
+  e.target.nextElementSibling.value = e.target.value
+}); */
